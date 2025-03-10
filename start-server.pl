3290855005ab8d448f29e1b64841c106d23d82e7
krzysckh <krzysckh@wp.pl>
2025-03-10 09:02:32 +0100

    fix start-server

--- start-server.pl
+++ start-server.pl
@@ -9,7 +9,7 @@ drop_privileges('nobody');
 
 while (1) {
   if (fork == 0) {
-    exec("/usr/local/bin/ol-rl", "-r", "server/leszcz-server.scm");
+    exec("/usr/bin/ol", "-r", "server/leszcz-server.scm");
   } else {
     wait
   }
